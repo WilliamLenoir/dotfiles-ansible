7482a164b83cdb5b19fbc8289c765695e260386e
William Lenoir <WilliamLenoir@users.noreply.github.com>
2024-04-21 14:53:07 +0200

    feat(path): move the required PATH vars to the .env
    - tested with `unset PATH`
    - all the paths needed to run the ansible playbook are specified

--- scripts/mac-easy-setup-python.sh
+++ scripts/mac-easy-setup-python.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# ensure that we have some variables in the PATH
-export PATH="/opt/homebrew/bin:$PATH"
-export PATH="/opt/homebrew/sbin:$PATH"
-
 brew update
 
 echo "Install python3 for mac user"
